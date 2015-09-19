fae473626e4cfe14c99021dbdf0cb8531ed9efb5
Ashutosh Nath Agarwal <ash7594@gmail.com>
2015-09-19 11:03:54 +0530

    enclosure rotation enabled

--- nefarious.js
+++ nefarious.js
@@ -1,4 +1,13 @@
 var global, shapeCreator, shapeEnclosure;
+var userControl = [];
+var userKeys = {
+	"left" : 37,
+	"right" : 39,
+	"top" : 38,
+	"down" : 40,
+	"rotateLeft" : 65,
+	"rotateRight" : 68,
+};
 var canvas = document.getElementById("canvas"),
 	ctx = canvas.getContext("2d");
 
@@ -9,9 +18,17 @@ var shapeEnclosureConstructor = function() {
 	this.height = 200;
 	this.dx = this.cx - this.width/2;
 	this.dy = this.cy - this.height/2;
+	this.rot = 0;
 };
 
 window.addEventListener("resize", onResize);
+window.addEventListener("keydown", function(e) {
+	userControl[e.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(e) {
+	userControl[e.keyCode] = false;
+});
 
 function onResize() {
 	global = {
@@ -34,17 +51,34 @@ function onResize() {
 
 onResize();
 
+function rotateEnclosure() {
+	if (userControl[userKeys.rotateLeft]) {
+		shapeEnclosure.rot--;
+	} else if (userControl[userKeys.rotateRight]) {
+		shapeEnclosure.rot++;
+	}
+}
+
 function drawEnclosure() {
+	ctx.beginPath();
+	ctx.save();
+	ctx.translate(shapeEnclosure.cx, shapeEnclosure.cy);
+	ctx.rotate(shapeEnclosure.rot / 180 * Math.PI);
 	ctx.lineWidth = shapeCreator.thickness;
-	ctx.rect(shapeEnclosure.dx, shapeEnclosure.dy, shapeEnclosure.width, shapeEnclosure.height);
+	ctx.rect(-1*shapeEnclosure.cx + shapeEnclosure.dx, -1*shapeEnclosure.cy + shapeEnclosure.dy, shapeEnclosure.width, shapeEnclosure.height);
 	ctx.stroke();
+	ctx.restore();
+	ctx.closePath();
 	//console.log("yo");
 }
 
 function frame () {
 	requestAnimationFrame(frame);
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 	//console.log("yo");
+	rotateEnclosure();
 	drawEnclosure();
 }
 
 frame();
+//setInterval(frame, 17);
